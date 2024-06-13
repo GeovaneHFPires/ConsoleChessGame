c1749a3e6e4da0358c46c3608ce10e6a02710861
Geovane <geovane.hau.franca.pires@gmail.com>
2024-06-13 20:56:07 -0300

    implementing Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,7 +22,7 @@ public class Program {
 		List<ChessPiece> captured = new ArrayList<ChessPiece>();
 
 
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -55,6 +55,9 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
+		
 
 	}
 
